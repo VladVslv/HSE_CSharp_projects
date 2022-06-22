7e3c275672b488e1ae0af39011aa1abe172675fb
VladVslv <108010118+VladVslv@users.noreply.github.com>
2022-06-22 17:57:53 +0300

    Add files via upload

---
+++ TransportApp/EKRLib/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKRLib
+{
+    /// <summary>
+    /// Класс, содержащий информация о машине.
+    /// </summary>
+    public class Car : Transport
+    {
+        /// <summary>
+        /// Конструктор для создания экземпляра класса.
+        /// </summary>
+        /// <param name="model">Модель.</param>
+        /// <param name="power">Мощность.</param>
+        public Car(string model, uint power) : base(model, power) { }
+
+        /// <summary>
+        /// Метод, который возвращает модель машины и звук, который она издаёт.
+        /// </summary>
+        /// <returns>Модель данной машины и звук(в строковом представлении).</returns>
+        public override string StartEngine()
+        {
+            return $"{Model}: Vroom";
+        }
+
+        /// <summary>
+        /// Метод для перевода экземпляра класса в строковое представление.
+        /// </summary>
+        /// <returns>Информация о данной машине в строковом представлении.</returns>
+        public override string ToString()
+        {
+            return "Car. " + base.ToString();
+        }
+    }
+}
---
+++ TransportApp/EKRLib/TransportException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKRLib
+{
+    /// <summary>
+    /// Собственный класс исключений.
+    /// </summary>
+    [Serializable]
+    public class TransportException : Exception
+    {
+        public TransportException() { }
+        public TransportException(string message) : base(message) { }
+        public TransportException(string message, Exception inner) : base(message, inner) { }
+        protected TransportException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
