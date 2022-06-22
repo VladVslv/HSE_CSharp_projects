96cfa361cd7584f714112a3e13315cef096752de
VladVslv <108010118+VladVslv@users.noreply.github.com>
2022-06-22 17:42:40 +0300

    Add files via upload

---
+++ MatrixCalculator/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		static void Main()
+		{
+			
+			double.TryParse(Console.ReadLine(), out double num);
+            Console.WriteLine(num/3.0);
+
+		}
+		
+
+	}
+}
